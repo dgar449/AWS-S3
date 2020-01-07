0215288e174e82ed22f8f18fd30cd758a664d72e
dgar449 <dgar449@aucklanduni.ac.nz>
2020-01-07 22:46:32 +1300

    Initial commit

---
+++ AWS Upload.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AWS Upload.aspx.cs" Inherits="AWS_S3.AWS_Upload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+   <form id="form1" runat="server">
+   
+      <div>
+         <h3> File Upload:</h3>
+         <br />
+         <asp:FileUpload ID="AWSFileUpload" runat="server" AllowMultiple="true" />
+         <br /><br />
+         <asp:Button ID="UploadButton" runat="server" onclick="UploadButton_Click"  Text="Upload" style="width:85px" />
+         <br /><br />
+         <asp:Label ID="UploadLabelMsg" runat="server" />
+         <br /><br />
+         <asp:DropDownList ID="AWSUploadKey" runat="server" />
+      </div>
+      
+   </form>
+</body>
+</html>
---
+++ FileUpload.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.S3.Transfer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Amazon.S3;
+
+namespace AWS_S3
+{
+    public class FileUpload
+    {
+        public string fUpload(string filePath, string s3Bucket, string newFileName)
+        {
+            try
+            {
+                TransferUtility fileTransferUtility = new
+                    TransferUtility(new AmazonS3Client(Amazon.RegionEndpoint.APSoutheast1));
+
+                //Specify object key name(newFileName) to upload in that path.
+                fileTransferUtility.Upload(filePath,
+                                          s3Bucket, newFileName);
+                System.Diagnostics.Debug.WriteLine("Upload completed!");
+
+                if (System.IO.File.Exists(filePath))
+                {
+                    System.IO.File.Delete(filePath);
+                }
+                return newFileName;
+            }
+            catch (AmazonS3Exception s3Exception)
+            {
+                throw s3Exception;
+            }
+        }
+    }
+}
